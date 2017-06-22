0645909b130bdb5bb207e75b8bbac4af33ce92d2
vimiix <i@vimiix.com>
2017-06-23 00:15:32 +0800

    Update sqlite models.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,32 @@
 # coding:utf-8
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=32, unique=True, verbose_name="用户名")
+class BaseModel(models.Model):
+    delete_Flag = models.CharField(max_length=32, verbose_name="删除标志")
+
+class User(BaseModel):
+    username = models.CharField(max_length=32, verbose_name="用户名")
     password = models.CharField(max_length=32, verbose_name="密码")
+    city = models.CharField(max_length=32, verbose_name="城市")
+    email = models.EmailField(verbose_name="邮箱")
+    phone = models.CharField(max_length=32, verbose_name="电话")
+
+class Group(BaseModel):
+    name = models.CharField(max_length=32, verbose_name="组名")
+    description = models.TextField(verbose_name="组描述")
+
+class Permission(BaseModel):
+    name = models.CharField(max_length=32, verbose_name="权限名称")
+    description = models.TextField(verbose_name="权限描述")
+
+class UserGroup(BaseModel):
+    user_id = models.IntegerField(verbose_name="用户ID")
+    group_id = models.IntegerField(verbose_name="组ID")
+
+class UserPermission(BaseModel):
+    user_id = models.IntegerField(verbose_name="用户ID")
+    permission_id = models.IntegerField(verbose_name="权限ID")
+
+class PermissionGroup(BaseModel):
+    permission_id = models.IntegerField(verbose_name="权限ID")
+    group_id = models.IntegerField(verbose_name="组ID")
--- users/views.py
+++ users/views.py
@@ -11,12 +11,10 @@ def repeat_user(username):
     :param username:
     :return:
     '''
-    status = {"status":"","result":""}
-    try:
-        user_data = User.objects.get(username = username)
+    status = {"status":""}
+    if User.objects.filter(username = username):
         status["status"] = "success"
-        #status["result"] = user_data
-    except Exception as e:
+    else:
         status["status"] = "error"
     return status
 
@@ -37,32 +35,20 @@ def register_repeat(request):
     else:
         return JsonResponse({"status":"","result":""})
 
-
 def register(request):
     if request.method == "POST" and request.POST:
-        name = request.POST["Username"]
-        passwd = request.POST["Password"]
-
-        #给密码hash加密
-        passwd = hash_password(passwd)
-
-        #新注册的用户数据入库
-        user = User()
-        user.username = name
-        user.password = passwd
-        user.save()
-
+        #新建一个用户模型
+        u = User()
+        #缓存用户POST上来的数据
+        u.username = request.POST["Username"]
+        u.password = hash_password(request.POST["Password"])  #密码加密入库
+        u.city = request.POST["City"]
+        u.email = request.POST["Email"]
+        u.phone = request.POST["Phone"]
+        #保存到数据库
+        u.save()
         return HttpResponseRedirect("/users/login")
-
     return render(request, 'register.html', locals())
-
-def loginValid(func):
-    def inner(request, *args, **kwargs):
-        name = request.COOKIES.get("name")  #获取cookie
-        if not name:
-            return HttpResponseRedirect("login")
-        return func(request, *args, **kwargs)
-    return inner
 
 def login(request):
     if request.method == "POST" and request.POST:
@@ -75,16 +61,25 @@ def login(request):
             dbPassWord = User.objects.get(username = name).password
             if passWd == dbPassWord:
                 response = HttpResponseRedirect("index")
-                response.set_cookie('name', name, max_age= 360)
+                response.set_cookie('name', name, max_age= 3600)
                 request.session["name"] = name
                 return response
             else:
-                return render(request, "login.html", locals())
+                return HttpResponseRedirect("login")
         else:
             return  render(request, "login.html", locals())
 
     return render(request, 'login.html', locals())
 
+def loginValid(func):
+    def inner(request, *args, **kwargs):
+        name = request.COOKIES.get("name")  #获取cookie
+        if not name:
+            return HttpResponseRedirect("login")
+        return func(request, *args, **kwargs)
+    return inner
+
 @loginValid
 def index(request):
-    return render(request, 'index.html' , locals())
+    user_name = request.COOKIES.get("name","")
+    return render(request, 'index.html', locals())
